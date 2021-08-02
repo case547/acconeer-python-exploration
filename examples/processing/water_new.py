c16e19c95e8fdb18ed37ccb41798c6c06f2facef
case547 <9521840+case547@users.noreply.github.com>
2021-08-02 15:48:57 +0100

    Added sensor configurability

--- examples/processing/water_new.py
+++ examples/processing/water_new.py
@@ -1,6 +1,7 @@
 import warnings
 from copy import copy
 from enum import Enum
+from attr import attr
 
 import numpy as np
 
@@ -8,17 +9,23 @@ import acconeer.exptool as et
 from argparse import ArgumentParser
 
 import json
+json_data = '{"downsampling_factor": 1, "gain": 0.2, "hw_accelerated_average_samples": 10, "maximize_signal_attenuation": "False", "noise_level_normalization": "True", "profile": "et.configs.EnvelopeServiceConfig.Profile.PROFILE_1", "range_interval": [0.1, 0.8], "repetition_mode": "et.configs.EnvelopeServiceConfig.RepetitionMode.SENSOR_DRIVEN", "running_average_factor": 0, "tx_disable": "False", "update_rate": 5}'
+json_as_py = json.loads(json_data)
 
 PEAK_MERGE_LIMIT_M = 0.005
 
 
 def main():
     """Needed to run detector from command line."""
+    global json_data
+    global json_as_py
+
     parser = ArgumentParser()
     parser.add_argument("socket_addr", help="connect via socket on given address (using json-based protocol)")
     args = parser.parse_args()
     
     client = et.SocketClient(args.socket_addr) # Raspberry Pi uses socket client
+    
     config = et.configs.EnvelopeServiceConfig() # picking envelope service
 
     sensor_config = get_sensor_config(config)
@@ -52,26 +59,57 @@ def main():
             peaks = np.take(processor.r, plot_data["found_peaks"]) * 100.0
             print(f"Sweep {index}: ", "{:.2f} cm".format(peaks[0]), "\n")
 
+        param = input('Enter parameter to change, "view", or "reconfig": ')
+
+        if param == 'view':
+            attr = input('Enter parameter to view: ')
+            print(getattr(config, attr))
+        elif param == 'reconfig':
+            print("Reconfiguring...")
+            client.disconnect()
+
+            sensor_config = get_sensor_config(config)
+            print('Reconfigured!')
+
+            session_info = client.setup_session(sensor_config) # also calls connect()
+            print("Session info:\n", session_info, "\n")
+
+            client.start_session()
+            processor = Processor(sensor_config, processing_config, session_info)
+
+            continue
+
+        elif hasattr(config, param):
+            new_val = input(f'Enter new value for {param}: ')
+            json_as_py[param] = eval(new_val)
+            json_data = json.dumps(json_as_py)
+            print(f'Reconfigure to apply: {param}, {getattr(config, param)} -> {eval(new_val)}')
+        else:
+            print('Invalid input.')
+
     print("Disconnecting...")
     client.disconnect()
 
 
 def get_sensor_config(config):
     """Define default sensor config and service to use."""
-    json_data = '{"profile": "et.configs.EnvelopeServiceConfig.Profile.PROFILE_1"}'
-    py_vals = json.loads(json_data)
+    for k, v in json_as_py.items():
+        try:
+            setattr(config, k, eval(json_as_py[k]))
+        except:
+            setattr(config, k, v)
 
-    config.downsampling_factor = 1 # must be 1, 2, or 4
-    config.gain = 0.2
-    config.hw_accelerated_average_samples = 10 # number of samples taken for single point in data [1,63]
-    config.maximize_signal_attenuation = False
-    config.noise_level_normalization = True
-    config.profile = eval(py_vals['profile'])
-    config.range_interval = [0.1, 0.6] # measurement range (metres)
-    config.repetition_mode = et.configs.EnvelopeServiceConfig.RepetitionMode.SENSOR_DRIVEN
-    config.running_average_factor = 0  # Use averaging in detector instead of in API
-    config.tx_disable = False # don't disable radio transmitter
-    config.update_rate = 5 # target measurement rate (Hz)
+    # config.downsampling_factor = 1 # must be 1, 2, or 4
+    # config.gain = 0.2
+    # config.hw_accelerated_average_samples = 10 # number of samples taken for single point in data [1,63]
+    # config.maximize_signal_attenuation = False
+    # config.noise_level_normalization = True
+    # config.profile = eval(py_vals['profile'])
+    # config.range_interval = [0.1, 0.6] # measurement range (metres)
+    # config.repetition_mode = et.configs.EnvelopeServiceConfig.RepetitionMode.SENSOR_DRIVEN
+    # config.running_average_factor = 0  # Use averaging in detector instead of in API
+    # config.tx_disable = False # don't disable radio transmitter
+    # config.update_rate = 5 # target measurement rate (Hz)
 
     return config
 
@@ -537,6 +575,8 @@ class ProcessingConfiguration(et.configbase.ProcessingConfig):
     nbr_average = 5.0
     threshold_type = ThresholdType.FIXED
     fixed_threshold = 1800
+    peak_sorting_type = PeakSorting.STRONGEST
+
 
     def check_sensor_config(self, sensor_config):
         alerts = {
